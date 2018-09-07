97a5f0e9acd18790e3847a2af77de77efe521511
Mario <chenliangboy@hotmail.com>
2018-09-07 23:45:46 +0800

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-app';
+  title = show('chenliang');
 }
+
+function show(name:string) {
+  return name;
+}
+
+
+
